8ea25721bcb07beb1c5a52e74c0afd31a28dd457
AlexandrTM <52867673+AlexandrTM@users.noreply.github.com>
2022-05-03 19:55:27 +0300

    Add files via upload

---
+++ Minecraft Kinda project Куликов Александр ИД23.4 Б1-20/code/pch.h
@@ -0,0 +1,29 @@
+#ifndef PCH_H
+#define PCH_H
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+#include <chrono>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <optional>
+#include <set>
+#include <cstdint>
+#include <algorithm>
+#include <fstream>
+#include <bitset>
+#include <array>
+#include <unordered_map>
+
+
+#endif
---
+++ Minecraft Kinda project Куликов Александр ИД23.4 Б1-20/shaders/compile.bat
@@ -0,0 +1,2 @@
+"O:\SDKforeverything\VulkanSDK\Bin\glslc.exe" shader.vert -o vert.spv
+"O:\SDKforeverything\VulkanSDK\Bin\glslc.exe" shader.frag -o frag.spv
